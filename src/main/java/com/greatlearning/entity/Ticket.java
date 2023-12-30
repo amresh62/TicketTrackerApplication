3b82221fb18712feaaf514588fb31d66c9125e31
Amresh Sinha <amarvijay62@outlook.com>
2023-12-30 17:18:43 +0530

    Implemented Lombok

--- src/main/java/com/greatlearning/entity/Ticket.java
+++ src/main/java/com/greatlearning/entity/Ticket.java
@@ -5,8 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Getter
+@Setter
+@ToString
 public class Ticket {
 
 	@Id
@@ -16,59 +22,6 @@ public class Ticket {
 	private String title;
 	private String description;
 	private String CreatedOn;
-	private String Content;
-
-	public Ticket() {
-
-	}
-
-	public Ticket(int id, String title, String description, String createdOn, String content) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.description = description;
-		CreatedOn = createdOn;
-		Content = content;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getCreatedOn() {
-		return CreatedOn;
-	}
-
-	public void setCreatedOn(String createdOn) {
-		CreatedOn = createdOn;
-	}
-
-	public String getContent() {
-		return Content;
-	}
-
-	public void setContent(String content) {
-		Content = content;
-	}
+	private String Content;	
 
 }
